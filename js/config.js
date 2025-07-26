84f3fe3c8867cdbf7a20ebff1955af30ad8a498e
Pritam <pritamchakrabortyshuvo@gmail.com>
2025-07-26 12:39:16 +0600

    Updated

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 
 window._config = {
   cognito: {
-    userPoolId: "ap-south-1_gBkCfunKG", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "7k19npil4kuovj6jgfssmgm044", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "ap-south-1_2g7RDoAZJ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1l64mbcji3ipiehoqvhnh8n45a", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "ap-south-l", // e.g. us-east-2
   },
   api: {
